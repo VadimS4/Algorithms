3ffb20348ef52946a54b6f907976f5fb229aa1c3
Vadim Stakhnyuk <47760426+VadimS4@users.noreply.github.com>
2019-06-18 09:59:19 -0700

    Update twoDigits.js

--- twoDigits.js
+++ twoDigits.js
@@ -3,5 +3,5 @@ function twoDigits(num) {
   let numString = num.toString()
   let newNum = numString.split('')
  
-  return parseInt(newNum[0]) + parseInt(newNum[1]);
+  return parseInt((newNum[0], 10)) + parseInt((newNum[1], 10));
 }
