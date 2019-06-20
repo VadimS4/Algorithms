c9a4687afc8843599292b76241119b693ec54b90
github username <github email address>
2019-06-20 09:41:33 -0700

    done

---
+++ largestOfFour.js
@@ -0,0 +1,15 @@
+function largestOfFour(array) {
+    
+    largestArray = []
+
+    for (let i = 0; i < array.length; i++) {
+        let largestNumber = 0;
+        for (let j = 0; j < array[i].length; j++) {
+            if (array[i][j] > largestNumber) {
+                largestNumber = array[i][j]
+            }
+        }
+        largestArray.push(largestNumber)
+    }
+    return largestArray;
+}
