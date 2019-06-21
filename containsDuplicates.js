415458720da5153b3cc7aaa2fe8123754341cf7c
github username <github email address>
2019-06-21 13:54:43 -0700

    completed

---
+++ containsDuplicates.js
@@ -0,0 +1,10 @@
+function containsDuplicates(input) {
+    for (let i = 0; i < input.length; i++) {
+        for (let j = i + 1; j < input.length; j++) {
+            if (input[i] === input[j]) {
+                return true;
+            }
+        }
+    }
+    return false
+}
